50b536861f16ce686aebd593da8d27946ada4058
Toma Gagne <toma.gagne.1@ulaval.ca>
2020-02-03 15:36:24 -0500

    working vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,40 @@
 <template>
   <div id="app">
-    <nav-menu></nav-menu>
-    <router-view></router-view>
+    <v-app id="inspire">
+      <v-navigation-drawer v-model="drawer" app clipped>
+        <nav-menu />
+      </v-navigation-drawer>
+
+      <v-app-bar app clipped-left>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+        <v-toolbar-title>Ubeat</v-toolbar-title>
+      </v-app-bar>
+
+      <v-content>
+        <v-container class="fill-height" fluid>
+          <router-view />
+        </v-container>
+      </v-content>
+    </v-app>
   </div>
 </template>
 
 <script>
-import VuetifyTest from "./components/VuetifyTest";
+import Navigation from "./components/Navigation";
 export default {
   name: "app",
   components: {
-    "nav-menu": VuetifyTest
-  }
+    "nav-menu": Navigation
+  },
+  props: {
+    source: String,
+  },
+  data: () => ({
+    drawer: null,
+  }),
+  created () {
+    this.$vuetify.theme.dark = true
+  },
 };
 </script>
 
-<style></style>
