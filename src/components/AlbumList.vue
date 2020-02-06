f4c67ee3daf60e32f9290166f8fb361d0cc146c9
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2020-02-06 10:30:14 -0500

    Working album list for artist page. Kind of screwed album page

---
+++ src/components/AlbumList.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-row>
+    <v-col>
+      <album
+        v-bind:albumInfo="album"
+        v-bind:key="album.collectionId"
+        v-for="album in albums"
+      >
+      </album>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import Album from "./Album.vue";
+
+export default {
+  name: "AlbumList",
+  props: ["albums"],
+  components: {
+    album: Album
+  }
+};
+</script>
