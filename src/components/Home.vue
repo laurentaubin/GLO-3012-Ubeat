2ba39aebeefb0d2291aeacc652d2c8ac16f8793b
remilevesque <remi.levesque.3@ulaval.ca>
2020-02-09 14:04:28 -0500

    home page 8/10

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,65 +1,127 @@
 <template>
-  <div>
+  <div class="all-in">
     <div class="premierDiv">
-      <p class="display-3" font-family="proxima-nova">Home</p>
+      <p class="display-3">Home</p>
     </div>
+    <div class="recentlyPlayed">
+      <p>Recently played</p>
+    </div>
+    <hr class="homeSeparator" />
+
     <div class="scrollmenu">
-      <img
-        style="max-width: 300px"
-        src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
-      />
-      <img
-        style="max-width: 300px"
-        src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
-      />
-      <img
-        style="max-width: 300px"
-        src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
-      />
-      <img
-        style="max-width: 300px"
-        src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
-      />
-      <img
-        style="max-width: 300px"
-        src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
-      />
-      <img
-        style="max-width: 300px"
-        src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
-      />
-      <img
-        style="max-width: 300px"
-        src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
-      />
-      <img
-        style="max-width: 300px"
-        src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
-      />
+      <span class="innerspan">
+        <img
+          class="imageShakira"
+          style="max-width: 300px; border-radius:50%"
+          src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
+        />
+        <btn class="boutonshakira" style="border-radius:60%">
+          <v-icon>mdi-play</v-icon>
+        </btn>
+
+        <p>Shakiraaaaa</p>
+      </span>
+      <span class="innerspan">
+        <img
+          style="border-radius:50%; max-width: 300px"
+          src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
+        />
+        <p>Shakira</p>
+      </span>
+      <span class="innerspan">
+        <img
+          style="max-width: 300px"
+          src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
+        />
+        <p>Shakira</p>
+      </span>
+      <span class="innerspan">
+        <img
+          style="max-width: 300px"
+          src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
+        />
+        <p>Shakira</p>
+      </span>
+      <span class="innerspan">
+        <img
+          style="max-width: 300px"
+          src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
+        />
+        <p>Shakira</p>
+      </span>
+      <span class="innerspan">
+        <img
+          style="max-width: 300px"
+          src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
+        />
+        <p>Shakira</p>
+      </span>
+      <span class="innerspan">
+        <img
+          style="max-width: 300px"
+          src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
+        />
+        <p>Shakira</p>
+      </span>
     </div>
+    <!-- deuxieme liste horizontale -->
     <div class="scrollmenu2">
-      <img
-        style="max-width: 300px"
-        src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
-      />
-      <img
-        style="max-width: 300px"
-        src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
-      />
-      <img
-        style="max-width: 300px"
-        src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
-      />
-      <img
-        style="max-width: 300px"
-        src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
-      />
-      <img
-        style="max-width: 300px"
-        src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
-      />
+      <span class="innerspan">
+        <img
+          class="imageShakira"
+          style="max-width: 300px; border-radius:50%"
+          src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
+        />
+        <btn class="boutonshakira" style="border-radius:60%">
+          <v-icon>mdi-play</v-icon>
+        </btn>
+
+        <p>Shakiraaaaa</p>
+      </span>
+      <span class="innerspan">
+        <img
+          style="border-radius:50%; max-width: 300px"
+          src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
+        />
+        <p>Shakira</p>
+      </span>
+      <span class="innerspan">
+        <img
+          style="max-width: 300px"
+          src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
+        />
+        <p>Shakira</p>
+      </span>
+      <span class="innerspan">
+        <img
+          style="max-width: 300px"
+          src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
+        />
+        <p>Shakira</p>
+      </span>
+      <span class="innerspan">
+        <img
+          style="max-width: 300px"
+          src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
+        />
+        <p>Shakira</p>
+      </span>
+      <span class="innerspan">
+        <img
+          style="max-width: 300px"
+          src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
+        />
+        <p>Shakira</p>
+      </span>
+      <span class="innerspan">
+        <img
+          style="max-width: 300px"
+          src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
+        />
+        <p>Shakira</p>
+      </span>
     </div>
-    <div class="scrollmenu3">
+    <!-- <div class="scrollmenu3">
       <img
         style="max-width: 300px"
         src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
@@ -80,29 +142,71 @@
         style="max-width: 300px"
         src="https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/1e/78/99/1e789927-28e6-c55a-204f-94ad4f7940ae/pr_source.png/570x570cc.jpg"
       />
-    </div>
+    </div>-->
   </div>
 </template>
 
 <script>
-import HouseMusicCard from "./HomeMusicCard";
+// import HouseMusicCard from "./HomeMusicCard";
 export default {
   name: "Home",
   components: ["HomeMusicCardList"] // voir comment on veut envoyer les données à la liste des cards
+  // playAudio: function() {
+  //   // if (this.audio !== null && !this.audio.paused) {
+  //   //   this.togglePlayButton(this.currentTrack);
+  //   //   this.audio.pause();
+  //   // }
+  //   // let currentTrack = document.getElementById(track.trackId);
+  //   let audio = new Audio(
+  //     "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview123/v4/b0/4a/3c/b04a3c05-d38d-9183-ef34-04c9b7fa8c84/mzaf_12378437392253983796.plus.aac.p.m4a"
+  //   );
+  //   audio.play();
+  // }
 };
 </script>
 
-
 <style>
+.all-in {
+  height: 1000px;
+}
+.imageShakira {
+  width: 100%;
+  height: auto;
+}
+
+.boutonshakira {
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  background-color: black;
+  color: white;
+  font-size: 10px;
+  padding: 12px 14px;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  font-size: 14px;
+  opacity: 50%;
+}
+
+.boutonshakira:hover {
+  opacity: 100%;
+}
+
 .scrollmenu {
   /* background-color: #333; */
+  position: relative;
+  top: 90px;
   overflow: scroll;
+  overflow-y: hidden;
   white-space: nowrap;
   max-width: 1650px;
+  max-height: 400px;
 }
-.scrollmenu img {
+.scrollmenu span {
   position: relative;
-  top: 70px;
   left: -50px;
   display: inline-block;
   color: white;
@@ -111,20 +215,30 @@ export default {
   padding: 2px;
   text-decoration: none;
 }
-.scrollmenu img:hover {
-  background-color: #ffffff;
+
+/* marche pas?? overlay sur la photo de l'artiste */
+.scrollmenu img:hover > .overlay {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  background-color: #000;
+  opacity: 0.5;
+  /* border-radius: 30px; */
 }
 
 .scrollmenu2 {
   /* background-color: #333; */
+  position: relative;
+  top: 70px;
   overflow: scroll;
+  overflow-y: hidden;
   white-space: nowrap;
   max-width: 1650px;
+  max-height: 400px;
 }
 
-.scrollmenu2 img {
+.scrollmenu2 span {
   position: relative;
-  top: 2px;
   left: -50px;
   display: inline-block;
   color: white;
@@ -147,7 +261,7 @@ export default {
 
 .scrollmenu3 img {
   position: relative;
-  top: -px;
+  top: 200px;
   left: -50px;
   display: inline-block;
   color: white;
@@ -161,11 +275,21 @@ export default {
 }
 .premierDiv {
   position: absolute;
-  top: 40px;
+  top: 80px;
 }
 .premierDiv p {
   font-family: "Courier New", Courier, monospace;
   padding-left: 20%;
+}
+.recentlyPlayed {
+  position: relative;
+  top: 140px;
+  left: 2%;
+}
+.recentlyPlayed p {
+  /* font-family: "Courier New", Courier, monospace; */
+  font-size: 25px;
+  opacity: 80%;
 }
 .deuxiemeDiv {
   position: absolute;
@@ -182,5 +306,12 @@ export default {
   overflow-x: auto;
   min-height: 300px;
   min-width: 1000;
+}
+.homeSeparator {
+  position: relative;
+  top: 130px;
+  width: 80%;
+  align-content: center;
+  left: 1%;
 }
 </style>
