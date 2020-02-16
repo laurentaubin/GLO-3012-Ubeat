e861202bcd0a353bcfb74cab0cf6040d78082618
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2020-02-16 16:04:49 -0500

    Added playlist display on /playlists/id

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>
+  <v-container>
     <h1>Playlist</h1>
-    <div>GLO-3102 Playlist</div>
-  </div>
+    <Track
+      v-bind:key="track.trackId"
+      v-bind:track="track"
+      v-bind:trackNumber="playlist.tracks.indexOf(track) + 1"
+      v-for="track in playlist.tracks"
+      v-on:select-track="emitTrackIdUp"
+      v-on:play-track="emitTrackUp"
+    />
+  </v-container>
 </template>
 
 <script>
+import Track from "./Track.vue";
+import { emitTrackIdUp, emitTrackUp } from "../utils/emitUtils";
+
 export default {
-  name: "Playlist"
+  name: "Playlist",
+  props: ["playlist"],
+  components: {
+    Track: Track
+  },
+  data: function() {
+    return {
+      updated: 0
+    };
+  },
+  methods: {
+    emitTrackUp: function(track) {
+      emitTrackUp(this, track);
+    },
+    emitTrackIdUp: function(trackId) {
+      emitTrackIdUp(this, trackId);
+    }
+  }
 };
 </script>
 
--- src/views/Playlist.vue
+++ src/views/Playlist.vue
@@ -1,13 +1,50 @@
 <template>
-  <Playlist />
+  <v-container>
+    <playlist
+      v-bind:playlist="playlist"
+      v-on:select-track="emitTrackIdUp"
+      v-on:play-track="emitTrackUp"
+    />
+  </v-container>
 </template>
 
 <script>
 import Playlist from "../components/Playlist";
+import { getPlaylist } from "../api/api.js";
+import { emitTrackIdUp, emitTrackUp } from "../utils/emitUtils";
+
 export default {
   name: "Playlist",
   components: {
     playlist: Playlist
+  },
+  data: function() {
+    return {
+      playlist: {
+        owner: {
+          email: "",
+          name: "",
+          id: 0
+        },
+        name: "",
+        tracks: [],
+        id: 0
+      }
+    };
+  },
+  created: async function() {
+    this.playlist = await this.getPlaylist();
+  },
+  methods: {
+    getPlaylist: async function() {
+      return await getPlaylist(this.$route.params.id);
+    },
+    emitTrackUp: function(track) {
+      emitTrackUp(this, track);
+    },
+    emitTrackIdUp: function(trackId) {
+      emitTrackIdUp(this, trackId);
+    }
   }
 };
 </script>
