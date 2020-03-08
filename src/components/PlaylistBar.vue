82c30bc9071730b34eb01bb65dc8b0fdbe7cb4f4
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2020-03-08 15:07:37 -0400

    Added custom scrollbar to playlist bar

--- src/components/PlaylistBar.vue
+++ src/components/PlaylistBar.vue
@@ -2,34 +2,43 @@
   <v-container class="px-0 mx-0">
     <v-subheader class="subtitle-1 font-weigth">Playlists</v-subheader>
     <!-- TODO Find a way to have dynamic max-height -->
-    <v-list dense style="max-height: 300px" class="px-0 mx-0 overflow-y-auto">
-      <v-list-item-group>
-        <v-list-item
-          class=""
-          v-for="playlist in playlists"
-          :key="playlist.id"
-          :to="`/playlist/${playlist.id}`"
-        >
-          <v-list-item-content class="py-0 my-0">
-            <v-list-item-title
-              class="subtitle-1 py-0 my-0"
-              v-text="playlist.name"
-            />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
+    <vue-custom-scrollbar class="scroll-area" :settings="settings">
+      <v-list dense class="px-0 mx-0 overflow-y-auto">
+        <v-list-item-group>
+          <v-list-item
+            class=""
+            v-for="playlist in playlists"
+            :key="playlist.id"
+            :to="`/playlist/${playlist.id}`"
+          >
+            <v-list-item-content class="py-0 my-0">
+              <v-list-item-title
+                class="subtitle-1 py-0 my-0"
+                v-text="playlist.name"
+              />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </vue-custom-scrollbar>
   </v-container>
 </template>
 
 <script>
+import vueCustomScrollbar from "vue-custom-scrollbar";
 import { getPlaylists } from "../api/api.js";
 
 export default {
   name: "PlaylistBar",
+  components: {
+    vueCustomScrollbar
+  },
   data: function() {
     return {
-      playlists: []
+      playlists: [],
+      settings: {
+        maxScrollbarLength: 60
+      }
     };
   },
   created() {
@@ -48,5 +57,11 @@ export default {
 v-list__tile {
   padding: 0;
   margin: 0;
+}
+.scroll-area {
+  position: relative;
+  margin: auto;
+  width: 225px;
+  height: 300px;
 }
 </style>
