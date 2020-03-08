b66954d2ca281952915e9e34e0def00bbf640d98
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2020-03-08 17:56:53 -0400

    Added onclick menu with dummy options

--- src/components/Track.vue
+++ src/components/Track.vue
@@ -1,35 +1,63 @@
 <template>
-  <v-row
-    v-bind:id="track.trackId"
-    :class="{
-      'track mx-0 px-0': $vuetify.breakpoint.xs,
-      track: $vuetify.breakpoint.smAndUp
-    }"
-    v-on:click="$emit('select-track', track.trackId)"
-  >
-    <v-col cols="1" class="d-flex align-center justify-center py-0">
-      <span class="track-nb">{{ trackNumber }}</span>
-      <v-btn class="play-btn" icon v-on:click="$emit('play-track', track)">
-        <v-icon>mdi-play</v-icon>
-      </v-btn>
-    </v-col>
-    <v-col cols="10" class="d-flex align-center">
-      <span>{{ track.trackName }}</span>
-    </v-col>
-    <v-col cols="1" class="d-flex align-center">{{
-      millisToMinutesAndSeconds(track.trackTimeMillis)
-    }}</v-col>
-  </v-row>
+  <v-hover v-slot:default="{ hover }">
+    <v-row
+      v-bind:id="track.trackId"
+      :class="{
+        'track mx-0 px-0': $vuetify.breakpoint.xs,
+        track: $vuetify.breakpoint.smAndUp
+      }"
+      v-on:click="trackSelected()"
+    >
+      <v-col cols="1" class="d-flex align-center justify-center py-0">
+        <span class="track-nb">{{ trackNumber }}</span>
+        <v-btn class="play-btn" icon v-on:click="$emit('play-track', track)">
+          <v-icon>mdi-play</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col cols="9" class="d-flex align-center">
+        <span>{{ track.trackName }}</span>
+      </v-col>
+      <v-col cols="1">
+        <div class="text-center">
+          <v-menu offset-y>
+            <template v-slot:activator="{ on }">
+              <a class="white--text" v-on="on">
+                <p v-if="hover" class="py-0 my-0">
+                  •••
+                </p>
+              </a>
+            </template>
+
+            <v-list width="175">
+              <v-list-item v-on:click="goToArtist()">
+                <v-list-item-title>Go to artist</v-list-item-title>
+              </v-list-item>
+              <v-list-item v-on:click="goToAlbum()">
+                <v-list-item-title>Go to album</v-list-item-title>
+              </v-list-item>
+              <v-list-item v-on:click="addToPlaylist()">
+                <v-list-item-title>Add to playlist </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+      </v-col>
+      <v-col cols="1" class="d-flex align-center">{{
+        millisToMinutesAndSeconds(track.trackTimeMillis)
+      }}</v-col>
+    </v-row>
+  </v-hover>
 </template>
 
 <script>
 export default {
   name: "Track",
   props: ["track", "trackNumber"],
-  data: function() {
-    return {};
-  },
+  data: () => ({}),
   methods: {
+    trackSelected() {
+      this.$emit("select-track", this.track.trackId);
+    },
     millisToMinutesAndSeconds(millis) {
       const minutes = Math.floor((millis / 1000 / 60) << 0);
       let seconds = Math.floor((millis / 1000) % 60);
@@ -49,7 +77,6 @@ export default {
 .track-nb {
   display: inline-block;
 }
-
 .play-btn {
   display: none !important;
   height: 30px;
