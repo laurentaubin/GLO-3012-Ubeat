908c105c1703700fb47fa418317b99caac0b0bd9
Toma Gagne <46379340+TomaGagne@users.noreply.github.com>
2020-04-15 17:49:46 -0400

    fix biography button position (#83)

--- src/components/artist/ArtistHeader.vue
+++ src/components/artist/ArtistHeader.vue
@@ -1,24 +1,25 @@
 <template>
-  <v-container class="d-block d-md-flex">
-    <div class="d-flex justify-center justify-md-start mb-3">
-      <img v-bind:src="artistArtworkUrl" class="artist-image" />
-    </div>
-    <div class="d-block ml-md-5 align-self-center">
-      <p class="font-weight-regular subtitle-2 text-center text-md-start mb-1">
-        {{ artist.primaryGenreName }}
-      </p>
-      <p
-        class="artist-name font-weight-bold text-center text-md-start"
-        :class="[$vuetify.breakpoint.smAndUp ? 'display-3' : 'display-1']"
-      >
-        {{ artist.artistName }}
-      </p>
-    </div>
-    <div class="d-flex justify-center justify-md-start mb-3">
-      <v-btn
-        v-on:click="this.getBiography"
-        color="rgb(88, 86, 214)"
-      >
+  <v-container>
+    <v-container class="d-block d-md-flex">
+      <div class="d-flex justify-center justify-md-start mb-3">
+        <img v-bind:src="artistArtworkUrl" class="artist-image" />
+      </div>
+      <div class="d-block ml-md-5 align-self-center">
+        <p
+          class="font-weight-regular subtitle-2 text-center text-md-start mb-1"
+        >
+          {{ artist.primaryGenreName }}
+        </p>
+        <p
+          class="artist-name font-weight-bold text-center text-md-start"
+          :class="[$vuetify.breakpoint.smAndUp ? 'display-3' : 'display-1']"
+        >
+          {{ artist.artistName }}
+        </p>
+      </div>
+    </v-container>
+    <div class="d-flex justify-center align-center mb-3">
+      <v-btn v-on:click="this.getBiography" color="rgb(88, 86, 214)">
         BIOGRAPHY
       </v-btn>
     </div>
