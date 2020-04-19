a773ac7bbe0743035f47132b4531b0e0038484bd
Toma Gagne <toma.gagne.1@ulaval.ca>
2020-04-19 17:11:35 -0400

    npm found 30$ airbnb

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,33 @@
     <v-container>
       <h1 class="font-weight-bold display-2 mb-3 pb-10">Home</h1>
     </v-container>
+    <v-container
+      v-if="loading"
+      style="width: 225px; height: 150px;"
+    >
+      <v-progress-circular
+        class="mt-6"
+        style="left: 38%;"
+        size="50"
+        :indeterminate="true"
+        color="rgb(88,86,214)"
+      ></v-progress-circular>
+    </v-container>
 
-    <v-container v-for="style in Object.keys(artistsByStyle)" v-bind:key="style">
+    <v-container
+      v-for="style in Object.keys(artistsByStyle)"
+      v-bind:key="style"
+    >
       <v-row no-gutters>
         <h1 class="title font-weight-bold">Top {{ style }} Artists For You</h1>
       </v-row>
       <v-divider class="pb-3" />
       <v-row no-gutter class="d-flex">
-        <v-card class="mr-3 mb-3" v-for="artist in artistsByStyle[style]" v-bind:key="artist.artistId">
+        <v-card
+          class="mr-3 mb-3"
+          v-for="artist in artistsByStyle[style]"
+          v-bind:key="artist.artistId"
+        >
           <v-row>
             <v-col class="d-flex mx-3 my-sm-1">
               <v-img
@@ -36,13 +55,14 @@
 
 <script>
 import { getArtists } from "../api/api";
-import {getArtistArtworkUrl} from "../utils/scraperUtils";
+import { getArtistArtworkUrl } from "../utils/scraperUtils";
 
 export default {
   name: "Home",
   data: () => {
     return {
-      artistsByStyle: {}
+      artistsByStyle: {},
+      loading: false
     };
   },
   async created() {
@@ -52,26 +72,32 @@ export default {
     getArtistsByStyle: async function() {
       let artistsByStyle = {};
       const results = await getArtists();
-      const artists = results.results;
-      // eslint-disable-next-line no-unused-vars
-      for (let artist of artists) {
-        if (artist.primaryGenreName in artistsByStyle) {
-          artistsByStyle[artist.primaryGenreName].push(artist);
-        } else {
-          artistsByStyle[artist.primaryGenreName] = [artist];
+      if (results !== null) {
+        this.loading = true;
+        const artists = results.results;
+        // eslint-disable-next-line no-unused-vars
+        for (let artist of artists) {
+          if (artist.primaryGenreName in artistsByStyle) {
+            artistsByStyle[artist.primaryGenreName].push(artist);
+          } else {
+            artistsByStyle[artist.primaryGenreName] = [artist];
+          }
         }
-      }
-      artistsByStyle = this.removeRandomStyles(artistsByStyle);
-      artistsByStyle = this.sortRecommendations(artistsByStyle);
+        artistsByStyle = this.removeRandomStyles(artistsByStyle);
+        artistsByStyle = this.sortRecommendations(artistsByStyle);
 
-      // eslint-disable-next-line no-unused-vars
-      for (let style of Object.keys(artistsByStyle)) {
-        artistsByStyle[style] = artistsByStyle[style].slice(0, 3);
-        for (let i = 0; i < artistsByStyle[style].length; i++) {
-          artistsByStyle[style][i]["artwork"] = await getArtistArtworkUrl(artistsByStyle[style][i].artistId);
+        // eslint-disable-next-line no-unused-vars
+        for (let style of Object.keys(artistsByStyle)) {
+          artistsByStyle[style] = artistsByStyle[style].slice(0, 3);
+          for (let i = 0; i < artistsByStyle[style].length; i++) {
+            artistsByStyle[style][i]["artwork"] = await getArtistArtworkUrl(
+              artistsByStyle[style][i].artistId
+            );
+          }
         }
+        this.loading = false;
+        return artistsByStyle;
       }
-      return artistsByStyle;
     },
     sortRecommendations: function(artistsByStyle) {
       Object.keys(artistsByStyle).forEach(key => {
